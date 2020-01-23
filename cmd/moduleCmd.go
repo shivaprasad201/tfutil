0a26b66c5556d5430f7c0e7d4a086f60efe79547
shivaprasad201 <shivaprasad1993@gmail.com>
2020-01-23 12:37:58 +0000

    add subcommands for config

--- cmd/moduleCmd.go
+++ cmd/moduleCmd.go
@@ -7,17 +7,40 @@ import (
 
 func init() {
 	rootCmd.AddCommand(cmdModule)
+	cmdModule.AddCommand(scmdGenerateConfig, scmdDescribe)
 }
 
 var cmdModule = &cobra.Command{
 	Use:   "module",
 	Short: "Work with teraform modules.",
-	Long: `Use 'module' command to work with terraform modules.`,
+	Long: `
+Use 'module' command to work with terraform modules.`,
 	Args: cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello from tfutil module.")
-		fmt.Println(args[0])
+		fmt.Println("Usage: type `tfutil module -h` `tfutil module --help` for options.")
 	},
 }
 
 // Subcommands section
+
+var scmdDescribe = &cobra.Command{
+	Use:   "describe",
+	Short: "Describes the terraform module.",
+	Long:  `
+Use 'tfutil module describe' command to get an overview of the terraform module.`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		// h.RenderTable(h.FindResource(h.ReadSource(source)))
+	},
+}
+
+var scmdGenerateConfig = &cobra.Command{
+	Use:   "generate-config",
+	Short: "Generates the terraform module configuration.",
+	Long:  `
+Use 'tfutil module generate-config' command to generate the terraform configuration from a module.`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		// h.RenderTable(h.FindResource(h.ReadSource(source)))
+	},
+}
